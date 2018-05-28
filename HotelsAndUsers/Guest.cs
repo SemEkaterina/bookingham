5bc6e25527dbb72cf0b26a0ac8ac167c815c9a9b
dav_dar1999 <dav_dar1999@mail.ru>
2018-05-29 01:01:29 +0300

    GuestClass

---
+++ HotelsAndUsers/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsAndUsers
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int MyProperty { get; set; }
+        public int Age { get; set; }
+        public string PassportId { get; set; }
+        public string PassportNumber { get; set; }
+        public int PreviousBookings { get; set; }
+    }
+}
