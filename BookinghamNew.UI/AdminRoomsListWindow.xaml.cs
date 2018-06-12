d3de222398aebf53803c79faea2f644b2da0101b
Ekaterina Semenova <nakoka11@mail.ru>
2018-06-12 22:02:30 +0300

    New Windows (admin)

---
+++ BookinghamNew.UI/AdminRoomsListWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookinghamNew.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminRoomsListWindow.xaml
+    /// </summary>
+    public partial class AdminRoomsListWindow : Window
+    {
+        public AdminRoomsListWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonShowGuests_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RoomsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ExitToHotelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
